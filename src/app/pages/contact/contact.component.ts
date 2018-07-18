1302dd5057ba85302e96c1033e08896e74390451
Edward Kim <taesup63@gmail.com>
2018-07-17 15:17:31 -1000

    Form Validation

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -10,7 +10,34 @@ export class ContactComponent {
     email: ''
   };
 
+  nameValid = false;
+  emailValid = false;
+
   constructor() { }
+
+  validateName(data) {
+    // validate exists
+    if (!this.formData.name) { this.nameValid = false; }
+    // validate length of at least 3
+    else if (this.formData.name.length < 3) { this.nameValid = false; }
+    // otherwise it's all good
+    else { this.nameValid = true; }
+  }
+
+  validateEmail() {
+    // validate exists
+    if (!this.formData.email) { this.emailValid = false; }
+    // validate length of at least 3
+    else if (this.formData.email.length < 3) { this.emailValid = false; }
+    // validate email includes at least one '@'
+    else if (!this.formData.email.includes('@')) { this.emailValid = false; }
+    // otherwise it's all good
+    else { this.emailValid = true; }
+  }
+
+  submitDisabled() {
+    return !(this.nameValid && this.emailValid);
+  }
 
   submitForm() {
     console.log('clicked');
